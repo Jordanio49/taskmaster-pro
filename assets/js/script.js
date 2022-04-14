5a780bbbd4f9d513f525ce3a577d02b761b2a3ab
Jordan Thomas <jthomas135@hotmail.com>
2022-04-14 11:33:09 -0700

    added drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,64 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
 
+  },
+  deactivate: function(event) {
+
+  },
+  over: function(event) {
+
+  },
+  out: function(event) {
+
+  },
+  update: function(event) {
+
+    var tempArr = [];
+
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+  },
+  out: function(event, ui) {
+  }
+});
 
 
 // modal was triggered
